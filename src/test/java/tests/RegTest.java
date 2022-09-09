4bfdb5e799817ab1c7b4ebc7c39e339a6c6ca552
darya <DariaSmit@yandex.ru>
2022-09-09 16:03:37 +0300

    added tests

---
+++ src/test/java/tests/RegTest.java
@@ -0,0 +1,43 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+
+public class RegTest {
+
+    @Test
+    void checkReqLogin() {
+        String body = "{ \"email\": \"eve.holt@reqres.in\"," +
+                              "\"password\": \"cityslicka\" }";
+
+        given()
+                .log().body()
+                .body(body)
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().body()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+    @Test
+    void checkReqLoginMissingPass() {
+        String body = "{ \"email\": \"eve.holt@reqres.in\"}";
+
+        given()
+                .log().body()
+                .body(body)
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().body()
+                .log().status()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+}
---
+++ src/test/java/tests/SelenoidTests.java
@@ -0,0 +1,86 @@
+package tests;
+
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.get;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertEquals;
+
+public class SelenoidTests {
+
+
+    @Test
+    void checkTotal() {
+        given()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .body("total", is(20));
+    }
+
+    @Test
+    void checkTotal2() {
+        given()
+                .log().all()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .body("total", is(20));
+    }
+
+    @Test
+    void checkTotal3() {
+        get("https://selenoid.autotests.cloud/status")
+                .then()
+                .body("browser.chrome", hasKey("100.0"));
+    }
+
+    @Test
+    void checkTotal33bad() {
+        Response response = get("https://selenoid.autotests.cloud/status")
+                .then()
+                .extract().response();
+        System.out.println(response.asString());
+        String expectedResponse = "{\"total\":20,\"used\":0,\"queued\":0,\"pending\":0  ,\"browsers\":" +
+                "{\"android\":{\"8.1\":{}},\"chrome\":{\"100.0\":{},\"99.0\":{}},\"chrome-mobile\":" +
+                "{\"86.0\":{}},\"firefox\":{\"97.0\":{},\"98.0\":{}},\"opera\":{\"84.0\":{},\"85.0\":{}}}}\n";
+
+        assertEquals(expectedResponse, response.asString());
+    }
+
+    @Test
+    void checkTotal33Good() {
+        Integer total = get("https://selenoid.autotests.cloud/status")
+                .then()
+                .extract().path("total");
+        System.out.println(total);
+        Integer expectedTotal = 20;
+
+        assertEquals(expectedTotal, total);
+    }
+
+    @Test
+    void checkTotalCode() {
+        get("https://selenoid.autotests.cloud/status")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void checkTotalCodeAu() {
+        get("https://selenoid.autotests.cloud/wd/hub/status")
+                .then()
+                .statusCode(401);
+    }
+
+    @Test
+    void checkTotalCodeAutTotal() {
+        given()
+                .auth().basic("user1", "1234")
+        .get("https://selenoid.autotests.cloud/wd/hub/status")
+                .then()
+                .statusCode(200);
+    }
+}
